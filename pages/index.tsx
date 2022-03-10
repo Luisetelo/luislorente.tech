98a8cc48ba4debcd99081240e6d7def35e5768b1
Luisetelo <luislorenteruiz@gmail.com>
2022-03-10 18:43:28 +0100

    Add chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ const IndexPage = () => (
   <>
     <h1>Hi! I'm Luisetelo 👋🏻</h1>
     <h1>and this is Buy me a Caf3 ☕️</h1>
-    <h2>The Web 3 alternative to buymeacoffee</h2>
+    <h2>my Web 3 alternative to buymeacoffee</h2>
   </>
 );
 
